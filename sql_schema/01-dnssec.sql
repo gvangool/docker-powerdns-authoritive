65afd8a11a60cee7ef89639558d902551c7fe787
Gert Van Gool <gertvangool@gmail.com>
2013-11-24 18:25:36 +0100

    Adds SQL schema for database

---
+++ sql_schema/01-dnssec.sql
@@ -0,0 +1,42 @@
+alter table records add ordername   VARCHAR(255);
+alter table records add auth bool;
+create index recordorder on records (domain_id, ordername text_pattern_ops);
+
+create table domainmetadata (
+     id         SERIAL PRIMARY KEY,
+     domain_id  INT REFERENCES domains(id) ON DELETE CASCADE,
+     kind       VARCHAR(16),
+     content    TEXT
+);
+
+create index domainidmetaindex on domainmetadata(domain_id);
+
+
+create table cryptokeys (
+     id         SERIAL PRIMARY KEY,
+     domain_id  INT REFERENCES domains(id) ON DELETE CASCADE,
+     flags      INT NOT NULL,
+     active     BOOL,
+     content    TEXT
+);
+create index domainidindex on cryptokeys(domain_id);
+
+
+GRANT ALL ON domainmetadata TO __PGSQL_USER__;
+GRANT ALL ON domainmetadata_id_seq TO __PGSQL_USER__;
+GRANT ALL ON cryptokeys TO __PGSQL_USER__;
+GRANT ALL ON cryptokeys_id_seq TO __PGSQL_USER__;
+
+create table tsigkeys (
+     id         SERIAL PRIMARY KEY,
+     name       VARCHAR(255),
+     algorithm  VARCHAR(50),
+     secret     VARCHAR(255),
+     constraint c_lowercase_name check (((name)::text = lower((name)::text)))
+);
+
+create unique index namealgoindex on tsigkeys(name, algorithm);
+
+GRANT ALL ON tsigkeys TO __PGSQL_USER__;
+GRANT ALL ON tsigkeys_id_seq TO __PGSQL_USER__;
+alter table records alter column type type VARCHAR(10);
---
+++ sql_schema/README.rst
@@ -0,0 +1,2 @@
+The base PostgreSQL schema's for PowerDNS, from
+<http://doc.powerdns.com/html/generic-mypgsql-backends.html#idp10146208>.
